49fe018933a490fc2c0d74a70bf9e1741f0a4558
Rana <mailmerana33@gmail.com>
2025-12-31 02:50:52 +0530

    updated s3 bucket name

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tf16112023"
+    bucket         = "terraform-artifacts-bucket-1"
     key            = "terraform.tfstate"
     region         = "us-east-1"
   }
