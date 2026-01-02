c13e3864c30b4958e29115745b25daf26394c7da
Rana <mailmerana33@gmail.com>
2026-01-02 17:07:50 +0530

    test

--- variables.tf
+++ variables.tf
@@ -14,7 +14,7 @@ variable "key_name" {
 }
 
 variable "s3_bucket_name" {
-  description = "Name of the S3 bucket for remote state"
+  description = "Name of the S3 bucket for remote state test"
   default = "terraform-artifacts-bucket-3"
 }
 
